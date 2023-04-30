c6a66a250f222f11480753c9702718630e263968
Mateusz Mackowiak <mateuszmackowiak+github@icloud.com>
2023-04-30 23:41:59 +0200

    Initial commit

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Mateusz Mackowiak on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
